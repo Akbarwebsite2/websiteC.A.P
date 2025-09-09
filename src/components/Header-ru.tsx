a91c0f3198f230c5855684c94653b55667764d3c
AkbarGTR <tiktokkomil1@gmail.com>
2025-09-09 17:45:48 +0500

    Переместить изображение text 2 максимально близко к логотипу с правильным отступом

--- src/components/Header-ru.tsx
+++ src/components/Header-ru.tsx
@@ -52,7 +52,7 @@ export const HeaderRu: React.FC = () => {
             <img 
               src="/text 2.png" 
               alt="C.A.P. Common Auto Parts" 
-             className="h-40 w-40 object-contain brightness-110 contrast-125 saturate-110 filter -ml-16"
+             className="h-40 w-40 object-contain brightness-110 contrast-125 saturate-110 filter -ml-20"
               style={{ 
                 imageRendering: 'crisp-edges',
                 WebkitImageRendering: 'crisp-edges',
