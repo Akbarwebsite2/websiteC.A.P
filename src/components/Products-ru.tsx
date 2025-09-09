f11f7e36f3c0395f89992dabf95f0b08f64962ab
AkbarGTR <tiktokkomil1@gmail.com>
2025-09-09 16:58:03 +0500

    Увеличить размер значков

--- src/components/Products-ru.tsx
+++ src/components/Products-ru.tsx
@@ -83,7 +83,7 @@ export const ProductsRu: React.FC = () => {
                   <img 
                     src={`/znak ${index + 1}.png`}
                     alt={`${product.title} icon`}
-                    className="w-16 h-16 object-cover filter brightness-110 contrast-110"
+                    className="w-24 h-24 object-cover filter brightness-110 contrast-110"
                     style={{ 
                       imageRendering: 'crisp-edges',
                       WebkitImageRendering: 'crisp-edges',
